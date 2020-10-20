8475bc039dcfce358b16a229cebaf7a9d9818d6d
emily <emily_nitzberg@yahoo.com>
2020-10-19 17:07:43 -0700

    Added ArrayList to the NumbersActivity to specify which numbers we will be creating a translation for. Also, created ArrayAdapters to easily generate the list of numbers on the page without taking up too much memory by using scrap views.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,12 +2,45 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers2);
+        setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbersArray = new ArrayList<String>();
+        numbersArray.add(0,"One");
+        numbersArray.add(1,"Two");
+        numbersArray.add(2,"Three");
+        numbersArray.add(3,"Four");
+        numbersArray.add(4,"Five");
+        numbersArray.add(5,"Six");
+        numbersArray.add(6,"Seven");
+        numbersArray.add(7,"Eight");
+        numbersArray.add(8,"Nine");
+        numbersArray.add(9,"Ten");
+
+        //Creating the LinearLayout view to display the ArrayList on screen
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//        for (int i = 0; i < numbersArray.size(); i++ ) {
+//            TextView wordView = new TextView(this);
+//            wordView.setText(numbersArray.get(i));
+//            rootView.addView(wordView);
+//        }
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbersArray);
+        ListView listView = (ListView) findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
     }
 }
